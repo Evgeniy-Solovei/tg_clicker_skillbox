55728820450c03342833e1d914beb3eedaebfd86
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-12-15 18:43:28 +0300

    updated bags, added menu in home page

--- static/assets/Portal-C6DSouZR.js
+++ static/assets/Portal-C6DSouZR.js
@@ -1,4 +1,4 @@
-import{h as f,d as u}from"./index-cDrDiH5_.js";var i={exports:{}};/*!
+import{h as f,d as u}from"./index-DPme1Txe.js";var i={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
