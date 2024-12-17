57a10ed6250892e512b86799f4a7f3c79ef85af7
maksimjoyharper <makksss1123@icloud.com>
2024-12-17 13:00:39 +0300

    feat: catalog link

--- static/assets/SlidingPanel-D7TIO_hb.js
+++ static/assets/SlidingPanel-D7TIO_hb.js
@@ -1,4 +1,4 @@
-import{g as R,h as I,r,j as d}from"./index-DO9z_rLG.js";var j={exports:{}};/*!
+import{g as R,h as I,r,j as d}from"./index-DvFT6pWS.js";var j={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
