1da6c6ea6fd07cd93d665a566bd03b4ece6978d1
maksimjoyharper <makksss1123@icloud.com>
2024-12-19 13:57:07 +0300

    feat: Loader

--- static/assets/SlidingPanel-l-4Rjq9I.js
+++ static/assets/SlidingPanel-l-4Rjq9I.js
@@ -1,4 +1,4 @@
-import{g as R,h as I,r,j as d}from"./index-BasPxLKV.js";var j={exports:{}};/*!
+import{g as R,h as I,r,j as d}from"./index-M08Ai4h4.js";var j={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
