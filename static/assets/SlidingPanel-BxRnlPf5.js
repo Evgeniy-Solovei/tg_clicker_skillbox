b41613e69436a99ef4bf6ef6076b087226bf0975
maksimjoyharper <makksss1123@icloud.com>
2024-12-19 07:58:57 +0300

    loader

--- static/assets/SlidingPanel-BxRnlPf5.js
+++ static/assets/SlidingPanel-BxRnlPf5.js
@@ -1,4 +1,4 @@
-import{g as R,h as I,r,j as d}from"./index-DQQYzHoX.js";var j={exports:{}};/*!
+import{g as R,h as I,r,j as d}from"./index-haEgMIv7.js";var j={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
